14a812231c50317947b9bd1a466796e605a7585d
katunariccorrea <vinikatunaric@gmail.com>
2017-05-31 21:26:11 -0300

    Alteração tirando acento da classe 10/05/2017

--- TrabIntegracaoContinua/src/trabintegracaocontinua/TrabIntegracaoContinua.java
+++ TrabIntegracaoContinua/src/trabintegracaocontinua/TrabIntegracaoContinua.java
@@ -15,7 +15,7 @@ public class TrabIntegracaoContinua {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Não consigo mandar pro github");
     }
     
 }
